07a27eaa871f99700f77f075ad86381991a457d9
alienatedDaddy <rian.w@mail.ugm.ac.id>
2019-04-24 18:00:40 +0700

    modified Heap

--- Semester II/Heap/Heap1.java
+++ Semester II/Heap/Heap1.java
@@ -35,6 +35,10 @@ class MaxHeap {
         return (currentSize == 0);
     }
 
+    public boolean isFull() {
+        return (currentSize == maxSize);
+    }
+
     public int getSize() {
         return this.currentSize;
     }
@@ -92,7 +96,7 @@ class MaxHeap {
         heapArray[index] = bottom;
     }
 
-    public Node replaceTop (int key) {
+    public Node replaceTop(int key) {
         Node top = heapArray[0];
         Node newTop = new Node(key);
         heapArray[0] = newTop;
@@ -100,16 +104,16 @@ class MaxHeap {
         return top;
     }
 
-    public int peek () {
+    public int peek() {
         return heapArray[0].getKey();
     }
 
     public int[] heapSort() {
         int[] result = new int[maxSize];
-        int counter = 0;
+        int counter = currentSize - 1;
         while (currentSize > 0) {
             result[counter] = extract().getKey();
-            counter++;
+            counter--;
         }
         return result;
     }
@@ -117,69 +121,84 @@ class MaxHeap {
 
 class Tester {
     public static void main(String[] args) {
+        int jumlah = 0;
         System.out.println("MaxHeap first try");
         Scanner sc = new Scanner(System.in);
-        System.out.print("\nMenu:\n1. Build random array of ints\n2. Build Max Heap\n3. Delete key from heap"
-                            + "\n4. Insert new key to heap\n5. Replace root of heap\n6. View array\n7. Heap Sort");
-        int menu = sc.nextInt();
-        switch (menu) {
-            case 1:
-                System.out.println("Building array of random integers\nEnter the number of elements:");
-                int jumlah = sc.nextInt();
-                MaxHeap h1 = new MaxHeap(jumlah);
-                int[] arrayAwal = new int[jumlah];
-                for (int i = 0; i < menu; i++) {
-                    arrayAwal[i] = ((int) (Math.random() * 100) + 1);
-                }
-                break;
-        
-            default:
-                break;
+        System.out.print("\nBuilding array of random integers\nEnter the number of elements:");
+        jumlah = sc.nextInt();
+        int[] arrayAwal = new int[jumlah];
+        MaxHeap h1 = new MaxHeap(jumlah);
+        for (int i = 0; i < jumlah; i++) {
+            arrayAwal[i] = ((int) (Math.random() * 100) + 1);
         }
-        System.out.print("Array awal: ");
         for (int i : arrayAwal) {
-            System.out.print(i + " ");
-
             h1.insert(i);
         }
 
-        System.out.println();
+        int menu = 0;
+        while (menu != 7) {
 
-        for (int i = 0; i < h1.getSize(); i++) {
-            System.out.print("[" + i + "]: " + h1.getArray()[i].getKey() + ",  ");
+            System.out.print("\nMenu:\n[1] Show generated array\n[2] Show Max Heap\n[3] Delete key from heap"
+                    + "\n[4] Insert new key to heap\n[5] Replace root of heap\n[6] Heap sort\n[7] Exit\n\n");
+            menu = sc.nextInt();
+            switch (menu) {
+            case 1:
+                System.out.println("Generated array: ");
+                for (int i = 0; i < arrayAwal.length; i++) {
+                    System.out.print("[" + i + "]: " + arrayAwal[i] + " ");
+                }
+                System.out.println();
+                break;
+            case 2:
+                System.out.println("Generated heap: ");
+
+                for (int i = 0; i < h1.getSize(); i++) {
+                    System.out.print("[" + i + "]: " + h1.getArray()[i].getKey() + " ");
+                }
+                System.out.println();
+                break;
+            case 3:
+                if (h1.isEmpty()) {
+                    System.out.println("Heap is empty, cannot delete key if there is no key!");
+                } else {
+                    System.out.println("Delete key");
+                    System.out.println(
+                            "\nThis will delete top-most heap member, which is the biggest value in max heap.");
+                    System.out.println("Deleted: " + (h1.extract().getKey()));
+                }
+                System.out.println();
+                break;
+            case 4:
+                System.out.print("\nInsert new key to heap.\nInsert new key: ");
+                int newKey = sc.nextInt();
+                h1.insert(newKey);
+                System.out.println(newKey + " inserted!");
+                System.out.println();
+                break;
+            case 5:
+                System.out.print("Replace root of heap, thus more efficient than deleting and then inserting new key."
+                        + "\nInsert new key: ");
+                int newRoot = sc.nextInt();
+                h1.replaceTop(newRoot);
+                System.out.println(newRoot + " replaced the previous root!");
+                System.out.println();
+                break;
+            case 6:
+                System.out.print("\nHeap sort. Please note that this will remove all of heap's elements!\nSorted array:\n");
+                int[] sorted = h1.heapSort();
+                for (int i = 0; i < sorted.length; i++) {
+                    if (sorted[i] == 0) {
+                        break;
+                    }
+                    System.out.print("[" + i + "]: " + sorted[i] + " ");
+                }
+                System.out.println();
+                break;
+            default:
+                break;
+            }
         }
-        System.out.println("\nDelete key");
-        System.out.println("Deleted: " + (h1.extract().getKey()));
-        System.out.println("Array: ");
-        for (int i = 0; i < h1.getSize(); i++) {
-            System.out.print("[" + i + "]: " + h1.getArray()[i].getKey() + ",  ");
-        }
-        System.out.println("\nDeleted: " + (h1.extract().getKey()));
-        System.out.println("Array: ");
-        for (int i = 0; i < h1.getSize(); i++) {
-            System.out.print("[" + i + "]: " + h1.getArray()[i].getKey() + ",  ");
-        }
-        System.out.println("\nDeleted: " + (h1.extract().getKey()));
-        System.out.println("Array: ");
-        for (int i = 0; i < h1.getSize(); i++) {
-            System.out.print("[" + i + "]: " + h1.getArray()[i].getKey() + ",  ");
-        }
-        System.out.print("\nReplace root with: ");
-        int newKey = (int) Math.random() * 100;
-        System.out.println(newKey);
-        h1.replaceTop(newKey);
-        System.out.println("Array: ");
-        for (int i = 0; i < h1.getSize(); i++) {
-            System.out.print("[" + i + "]: " + h1.getArray()[i].getKey() + ",  ");
-        }
-        System.out.print("\nReplace root with: ");
-        newKey = ((int) (Math.random() * 100) + 1);
-        System.out.println(newKey);
-        h1.replaceTop(newKey);
-        System.out.println("Array: ");
-        for (int i = 0; i < h1.getSize(); i++) {
-            System.out.print("[" + i + "]: " + h1.getArray()[i].getKey() + ",  ");
-        }
+        System.out.println("Thank you!");
         sc.close();
     }
 }
